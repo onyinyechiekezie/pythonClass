0a019266e9b3b187fded649cd1d8d1e78879ae88
0nyinye <onyinyeekezi2017@gmail.com>
2024-11-02 17:59:37 +0100

    chapter3(python)

---
+++ chapterExercises/chapter3/exesrciseTwo.py
@@ -0,0 +1,9 @@
+a = b = 7 means that he value "7" is assigned to both a an b
+That is a = 7 and b = 7
+
+print('a = ', a,  '\nb =',  b)
+This prints a = 7
+b = 7
+
+There's nothing wrong with the code.
+
